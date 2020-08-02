cf76ba46403da53115c1f96e33cd864e93e1614b
Kazu-03 <66908956+Kazu-03@users.noreply.github.com>
2020-08-03 00:20:07 +0900

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,9 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.14.1"
+lock '~> 3.14.1'
 
-set :application, "chat-space"
+set :application, 'chat-space'
 
-set :repo_url, "git@github.com:Kazu-03/chat-space.git"
+set :repo_url, 'git@github.com:Kazu-03/chat-space.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
